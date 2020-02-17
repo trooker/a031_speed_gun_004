9286ce2657ecac5be03a2fccf3f376ed63b0dc40
Zack Usso <qed@abbottanp.com>
2020-02-17 16:16:15 -0500

    Delete a031_speed_gun_004/Release/core/core/WString.cpp.d

--- a031_speed_gun_004/Release/core/core/WString.cpp.d
+++
@@ -1,5 +0,0 @@
-core/core/WString.cpp.o: \
- /opt/Sloeber/arduinoPlugin/packages/arduino/hardware/avr/1.8.2/cores/arduino/WString.cpp \
- /opt/Sloeber/arduinoPlugin/packages/arduino/hardware/avr/1.8.2/cores/arduino/WString.h
-
-/opt/Sloeber/arduinoPlugin/packages/arduino/hardware/avr/1.8.2/cores/arduino/WString.h:
